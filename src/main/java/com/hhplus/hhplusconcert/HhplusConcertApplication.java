37fb4b55e1e33e3ec6703a2b3511f19983187adc
gt.park <smileboy0014@gmail.com>
2024-07-02 23:51:11 +0900

    first commit

---
+++ src/main/java/com/hhplus/hhplusconcert/HhplusConcertApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplusconcert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusConcertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusConcertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hhplus/hhplusconcert/HhplusConcertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplusconcert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusConcertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
