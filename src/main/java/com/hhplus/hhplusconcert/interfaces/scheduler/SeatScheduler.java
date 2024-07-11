b0715bc62f92477ee637a82a3c9584809e36c1ea
gt.park <smileboy0014@gmail.com>
2024-07-12 02:44:09 +0900

    feat: 스케줄러 추가

---
+++ src/main/java/com/hhplus/hhplusconcert/interfaces/scheduler/SeatScheduler.java
@@ -0,0 +1,22 @@
+package com.hhplus.hhplusconcert.interfaces.scheduler;
+
+import com.hhplus.hhplusconcert.application.reservation.ReservationFacade;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SeatScheduler { //좌석 관련 스케줄러
+
+    private final ReservationFacade reservationFacade;
+
+    @Scheduled(fixedRate = 5000) // 매 5초마다 스케줄러 실행
+    public void checkOccupiedSeat() {
+        log.info("5초 마다 예약 후, 5분이 지났는지 체크하는 스케줄러 실행");
+        reservationFacade.checkOccupiedSeat(); //좌석을 계속 점유할 수 있는지 확인(최대 5분)
+    }
+}
