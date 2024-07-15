9fa6eece965dd64fb2e6c57520e62abed0bca134
gt.park <smileboy0014@gmail.com>
2024-07-15 16:39:11 +0900

    지난 피드백 반영: Entity들의 status String -> enum 타입으로 변경

--- src/main/java/com/hhplus/hhplusconcert/domain/concert/enums/ReservationStatus.java
+++ src/main/java/com/hhplus/hhplusconcert/domain/concert/enums/ReservationStatus.java
@@ -1,16 +1,8 @@
 package com.hhplus.hhplusconcert.domain.concert.enums;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Getter
 public enum ReservationStatus {
+    COMPLETED, //예약 완료
+    TEMPORARY_RESERVED, // 임시 예약 완료
+    CANCEL // 예약 취소
 
-    CANCEL("cancel"),
-    PROGRESSING("progressing"),
-    COMPLETED("completed");
-
-
-    private final String status;
 }
