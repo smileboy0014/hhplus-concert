ba94e805faefc44831f05c6a87f1670ff255ac07
gt.park <smileboy0014@gmail.com>
2024-07-19 02:49:51 +0900

    feat: 멘토링 피드백 반영, ErrorHandler 변경

--- src/main/java/com/hhplus/hhplusconcert/interfaces/controller/common/dto/ApiResultResponse.java
+++ src/main/java/com/hhplus/hhplusconcert/interfaces/controller/common/dto/ApiResultResponse.java
@@ -3,19 +3,19 @@ package com.hhplus.hhplusconcert.interfaces.controller.common.dto;
 
 import org.springframework.http.HttpStatus;
 
-public record ApiResultResponse<T>(HttpStatus status, String msg, T data) {
+public record ApiResultResponse<T>(HttpStatus status, boolean success, String msg, T data) {
 
-    public static <T> ApiResultResponse<T> of(HttpStatus httpStatus, String message, T data) {
-        return new ApiResultResponse<>(httpStatus, message, data);
+    public static <T> ApiResultResponse<T> of(HttpStatus httpStatus, boolean success, String message, T data) {
+        return new ApiResultResponse<>(httpStatus, success, message, data);
     }
 
-    public static <T> ApiResultResponse<T> of(HttpStatus httpStatus, T data) {
-        return new ApiResultResponse<>(httpStatus, httpStatus.name(), data);
+    public static <T> ApiResultResponse<T> of(HttpStatus httpStatus, boolean success, T data) {
+        return new ApiResultResponse<>(httpStatus, success, httpStatus.name(), data);
     }
 
 
     public static <T> ApiResultResponse<T> ok(T data) {
-        return of(HttpStatus.OK, data);
+        return of(HttpStatus.OK, true, data);
     }
 
 
