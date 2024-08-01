b7a3b2d056a19144232d05ec510cbd11c12b8c28
gt.park <smileboy0014@gmail.com>
2024-08-02 06:20:51 +0900

    feat: 부하테스트 툴 추가

---
+++ docker/load-test/locustfile-issueV1.py
@@ -0,0 +1,16 @@
+import random
+from locust import task, FastHttpUser, stats
+
+stats.PERCENTILES_TO_CHART = [0.95, 0.99]
+
+class CouponIssueV1(FastHttpUser):
+    connection_timeout = 10.0
+    network_timeout = 10.0
+
+    @task
+    def issue(self):
+        payload = {
+            "userId": random.randint(1, 10000000),
+        }
+        with self.rest("POST", "/api/v1/queues/token", json=payload):
+            pass
