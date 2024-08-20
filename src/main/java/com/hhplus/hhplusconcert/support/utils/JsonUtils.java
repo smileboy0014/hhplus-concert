51d1f905da10af0e64061ad8b2bb4624c3e23b0b
gt.park <smileboy0014@gmail.com>
2024-08-20 17:23:11 +0900

    refector: Json 관련 util 추가

---
+++ src/main/java/com/hhplus/hhplusconcert/support/utils/JsonUtils.java
@@ -0,0 +1,42 @@
+package com.hhplus.hhplusconcert.support.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Slf4j
+public class JsonUtils {
+
+    private static final ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public static String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            log.error("Error serializing object to JSON: {}", e.getMessage());
+            return "";
+        }
+    }
+
+    public static <T> T toObject(String jsonModel, Class<T> targetClass) {
+        try {
+            return objectMapper.readValue(jsonModel, targetClass);
+        } catch (RuntimeException | JsonProcessingException e) {
+            log.error("Error deserializing JSON to object: {}", e.getMessage());
+            return null;
+        }
+    }
+}
